2560226d19c8f8ea0ec83db259b7564778cefe90
serdar sezgin <serdar.sezgin@outlook.com>
2025-02-18 08:16:16 +0300

    Cari sil

--- repos/KobiFinans/Kobi_v1/FrmCariEkle.Designer.cs
+++ repos/KobiFinans/Kobi_v1/FrmCariEkle.Designer.cs
@@ -28,36 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtCariAdi = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtEposta = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtVergiNo = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.txtBorc = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.txtAlacak = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnTemizle = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtCariAdi
             // 
-            this.textBox1.Location = new System.Drawing.Point(92, 77);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(152, 28);
-            this.textBox1.TabIndex = 0;
+            this.txtCariAdi.Location = new System.Drawing.Point(92, 77);
+            this.txtCariAdi.Name = "txtCariAdi";
+            this.txtCariAdi.Size = new System.Drawing.Size(152, 28);
+            this.txtCariAdi.TabIndex = 1;
             // 
             // label1
             // 
@@ -68,158 +69,178 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Cari Adı";
             // 
-            // textBox2
+            // txtTelefon
             // 
-            this.textBox2.Location = new System.Drawing.Point(92, 143);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(152, 28);
-            this.textBox2.TabIndex = 0;
+            this.txtTelefon.Location = new System.Drawing.Point(92, 120);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(152, 28);
+            this.txtTelefon.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 146);
+            this.label2.Location = new System.Drawing.Point(12, 123);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 22);
             this.label2.TabIndex = 1;
             this.label2.Text = "Telefon";
             // 
-            // textBox3
+            // txtEposta
             // 
-            this.textBox3.Location = new System.Drawing.Point(92, 209);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(152, 28);
-            this.textBox3.TabIndex = 0;
+            this.txtEposta.Location = new System.Drawing.Point(92, 168);
+            this.txtEposta.Name = "txtEposta";
+            this.txtEposta.Size = new System.Drawing.Size(152, 28);
+            this.txtEposta.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 212);
+            this.label3.Location = new System.Drawing.Point(12, 171);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(74, 22);
             this.label3.TabIndex = 1;
             this.label3.Text = "E-Posta";
             // 
-            // textBox4
+            // txtAdres
             // 
-            this.textBox4.Location = new System.Drawing.Point(92, 260);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(152, 75);
-            this.textBox4.TabIndex = 0;
+            this.txtAdres.Location = new System.Drawing.Point(92, 221);
+            this.txtAdres.Multiline = true;
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(152, 75);
+            this.txtAdres.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 260);
+            this.label4.Location = new System.Drawing.Point(12, 221);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(57, 22);
             this.label4.TabIndex = 1;
             this.label4.Text = "Adres";
             // 
-            // textBox5
+            // txtVergiNo
             // 
-            this.textBox5.Location = new System.Drawing.Point(92, 369);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(152, 28);
-            this.textBox5.TabIndex = 0;
+            this.txtVergiNo.Location = new System.Drawing.Point(92, 319);
+            this.txtVergiNo.Name = "txtVergiNo";
+            this.txtVergiNo.Size = new System.Drawing.Size(152, 28);
+            this.txtVergiNo.TabIndex = 5;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 372);
+            this.label5.Location = new System.Drawing.Point(12, 322);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(80, 22);
             this.label5.TabIndex = 1;
             this.label5.Text = "Vergi No";
             // 
-            // textBox6
+            // txtBorc
             // 
-            this.textBox6.Location = new System.Drawing.Point(92, 419);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(152, 28);
-            this.textBox6.TabIndex = 0;
+            this.txtBorc.Location = new System.Drawing.Point(92, 369);
+            this.txtBorc.Name = "txtBorc";
+            this.txtBorc.Size = new System.Drawing.Size(152, 28);
+            this.txtBorc.TabIndex = 6;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 422);
+            this.label6.Location = new System.Drawing.Point(12, 372);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(47, 22);
             this.label6.TabIndex = 1;
             this.label6.Text = "Borç";
             // 
-            // textBox7
+            // txtAlacak
             // 
-            this.textBox7.Location = new System.Drawing.Point(92, 469);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(152, 28);
-            this.textBox7.TabIndex = 0;
+            this.txtAlacak.Location = new System.Drawing.Point(92, 419);
+            this.txtAlacak.Name = "txtAlacak";
+            this.txtAlacak.Size = new System.Drawing.Size(152, 28);
+            this.txtAlacak.TabIndex = 7;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 472);
+            this.label7.Location = new System.Drawing.Point(12, 422);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(64, 22);
             this.label7.TabIndex = 1;
             this.label7.Text = "Alacak";
             // 
-            // button1
+            // btnEkle
             // 
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 36);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Ekle";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEkle.Location = new System.Drawing.Point(6, 20);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(84, 36);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
             // 
-            // flowLayoutPanel1
+            // btnGuncelle
             // 
-            this.flowLayoutPanel1.Controls.Add(this.button1);
-            this.flowLayoutPanel1.Controls.Add(this.button2);
-            this.flowLayoutPanel1.Controls.Add(this.button4);
-            this.flowLayoutPanel1.Controls.Add(this.button3);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(250, 15);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(620, 56);
-            this.flowLayoutPanel1.TabIndex = 3;
+            this.btnGuncelle.Location = new System.Drawing.Point(96, 20);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(90, 36);
+            this.btnGuncelle.TabIndex = 1;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
             // 
-            // button2
+            // btnListele
             // 
-            this.button2.Location = new System.Drawing.Point(93, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 36);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Güncelle";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnListele.Location = new System.Drawing.Point(282, 20);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(90, 36);
+            this.btnListele.TabIndex = 3;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
             // 
-            // button3
+            // btnSil
             // 
-            this.button3.Location = new System.Drawing.Point(279, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(90, 36);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Listele";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(189, 3);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(84, 36);
-            this.button4.TabIndex = 2;
-            this.button4.Text = "Sil";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnSil.Location = new System.Drawing.Point(192, 20);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(84, 36);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(250, 77);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(620, 424);
-            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(620, 370);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnTemizle);
+            this.groupBox1.Controls.Add(this.btnListele);
+            this.groupBox1.Controls.Add(this.btnSil);
+            this.groupBox1.Controls.Add(this.btnGuncelle);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Location = new System.Drawing.Point(253, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(617, 66);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(378, 20);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(90, 36);
+            this.btnTemizle.TabIndex = 3;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
             // 
             // FrmCariEkle
             // 
@@ -227,28 +248,28 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(882, 553);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtAlacak);
+            this.Controls.Add(this.txtBorc);
+            this.Controls.Add(this.txtVergiNo);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.txtEposta);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtCariAdi);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "FrmCariEkle";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cari Ekle";
-            this.flowLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -256,25 +277,26 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtCariAdi;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtTelefon;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtEposta;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtAdres;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtVergiNo;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox txtBorc;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox txtAlacak;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnListele;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnTemizle;
     }
 }
--- repos/KobiFinans/Kobi_v1/FrmCariEkle.cs
+++ repos/KobiFinans/Kobi_v1/FrmCariEkle.cs
@@ -16,8 +16,167 @@ namespace Kobi_v1
         public FrmCariEkle()
         {
             InitializeComponent();
+            Listele();
         }
         SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-NFG00P8\\SQLEXPRESS;Initial Catalog=KobiFinans;Integrated Security=True");
         string sorguEkle = "INSERT INTO Cari (CariAdi, Telefon, Email, Adres, VergiNo, Borc, Alacak)  VALUES (@CariAdi, @Telefon, @Email, @Adres, @VergiNo, @Borc, @Alacak)";
+        string sorguGuncelle = "UPDATE Cari SET CariAdi=@CariAdi, Telefon=@Telefon, Email=@Email, Adres=@Adres, VergiNo=@VergiNo, Borc=@Borc, Alacak=@Alacak WHERE CariID=@CariId";
+        string sorguSil = "DELETE FROM Cari WHERE CariID=@CariId";
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(txtCariAdi.Text))
+                {
+                    MessageBox.Show("Cari Adı Boş Bırakılamaz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                    return;
+                }
+                if(!decimal.TryParse(txtBorc.Text, out _)||!decimal.TryParse(txtAlacak.Text,out _))
+                {
+                    MessageBox.Show("Borç ve Alacak Alanına Sayısal Değer Giriniz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (baglanti.State == ConnectionState.Closed)
+                    baglanti.Open();
+                SqlCommand kmt = new SqlCommand(sorguEkle, baglanti);
+                kmt.Parameters.AddWithValue("@CariAdi", txtCariAdi.Text);
+                kmt.Parameters.AddWithValue("@Telefon", txtTelefon.Text);
+                kmt.Parameters.AddWithValue("@Email", txtEposta.Text);
+                kmt.Parameters.AddWithValue("@Adres", txtAdres.Text);
+                kmt.Parameters.AddWithValue("@VergiNo", txtVergiNo.Text);
+                kmt.Parameters.AddWithValue("@Borc", Convert.ToDecimal(txtBorc.Text));
+                kmt.Parameters.AddWithValue("@Alacak", Convert.ToDecimal(txtAlacak.Text));
+                kmt.ExecuteNonQuery();
+                MessageBox.Show("Cari Eklendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Listele();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                baglanti.Close();
+            }
+        }
+        private void Listele()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select * from Cari", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            temizle();
+
+        }
+        void temizle()
+        {
+            txtCariAdi.Clear();
+            txtTelefon.Clear();
+            txtEposta.Clear();
+            txtAdres.Clear();
+            txtVergiNo.Clear();
+            txtBorc.Clear();
+            txtAlacak.Clear();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >=0)// Geçerli bir Satır Seçilirse
+            {
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+                txtCariAdi.Text = row.Cells["CariAdi"].Value.ToString();
+                txtTelefon.Text = row.Cells["Telefon"].Value.ToString();
+                txtEposta.Text = row.Cells["Email"].Value.ToString();
+                txtAdres.Text = row.Cells["Adres"].Value.ToString();
+                txtVergiNo.Text = row.Cells["VergiNo"].Value.ToString();
+                txtBorc.Text = row.Cells["Borc"].Value.ToString();
+                txtAlacak.Text = row.Cells["Alacak"].Value.ToString();
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtCariAdi.Text))
+                {
+                    MessageBox.Show("Cari Adı Boş Bırakılamaz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (!decimal.TryParse(txtBorc.Text, out _) || !decimal.TryParse(txtAlacak.Text, out _))
+                {
+                    MessageBox.Show("Borç ve Alacak Alanına Sayısal Değer Giriniz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (baglanti.State == ConnectionState.Closed)
+                    baglanti.Open();
+                SqlCommand kmt = new SqlCommand(sorguGuncelle, baglanti);
+                kmt.Parameters.AddWithValue("@CariAdi", txtCariAdi.Text);
+                kmt.Parameters.AddWithValue("@Telefon", txtTelefon.Text);
+                kmt.Parameters.AddWithValue("@Email", txtEposta.Text);
+                kmt.Parameters.AddWithValue("@Adres", txtAdres.Text);
+                kmt.Parameters.AddWithValue("@VergiNo", txtVergiNo.Text);
+                kmt.Parameters.AddWithValue("@Borc", Convert.ToDecimal(txtBorc.Text));
+                kmt.Parameters.AddWithValue("@Alacak", Convert.ToDecimal(txtAlacak.Text));
+                kmt.Parameters.AddWithValue("@CariId", dataGridView1.CurrentRow.Cells["CariID"].Value);
+                kmt.ExecuteNonQuery();
+                MessageBox.Show("Cari Güncellendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Listele();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                baglanti.Close();
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(dataGridView1.CurrentRow == null)
+                {
+                    MessageBox.Show("Silinecek Cariyi Seçiniz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                DialogResult sonuc = MessageBox.Show("Cari Kaydı Silinsin mi?", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if(sonuc == DialogResult.Yes) 
+                {
+                if (baglanti.State == ConnectionState.Closed)
+                    baglanti.Open();                                  
+                    SqlCommand kmt = new SqlCommand(sorguSil, baglanti);
+                    kmt.Parameters.AddWithValue("@CariId", dataGridView1.CurrentRow.Cells["CariID"].Value);                    
+                    kmt.ExecuteNonQuery();
+
+                    MessageBox.Show("Cari Silindi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Listele();
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                baglanti.Close();
+            }
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+
+        }
     }
+    
 }
+
